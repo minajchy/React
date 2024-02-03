5261a031808a7b80c1d8abadcf1004a17cca33a4
minajchy <minajchy.123@gmail.com>
2024-02-04 00:39:36 +0530

    Add myfirst/src/App.js and myfirst/src/components/ExpenseItems.js

---
+++ myfirst/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import ExpenseItem from "./components/ExpenseItems";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ myfirst/src/components/ExpenseItems.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Expense Item!</h2>
+      </div>
+      <div>Food Rs 10</div>
+      <div>Petrol Rs 100</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
